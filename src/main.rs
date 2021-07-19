a92525472388e84f3a30977ba7434f0a7a8c8dce
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2021-07-19 17:37:33 -0400

    Added warp functionality

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,49 @@ use std::io::BufReader;
 use std::thread::sleep;
 use std::time::Duration;
 use std::str::Split;
+use std::collections::HashMap;
 
 fn main() {
-    let path = Path::new("script.txt");
+    let world = open_world("start");
 
-    let file = match File::open(&path) {
-        Err(error) => panic!("Oh no! We couldn't open the script!\n\nShutting down..."),
-        Ok(file) => file
-    };
-
-    let buffer = BufReader::new(file);
-    let script: Vec<String> = buffer.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect();
-
-    game_loop(script);
+    game_loop(world);
 
     println!("\nShutting down...");
     return;
 }
 
-fn game_loop(script: Vec<String>) {
-    let mut location = 0;
+fn open_world(world: &str) -> Vec<String> {
+    let str_path = format!("../script/{}.txt", world);
+    let path = Path::new(&str_path);
+
+    let file = match File::open(&path) {
+        Err(_) => panic!("Oh no! We couldn't open the script!\n\nShutting down..."),
+        Ok(file) => file
+    };
+
+    let buffer = BufReader::new(file);
+    let opened: Vec<String> = buffer.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect();
+    
+    return opened;
+}
+
+struct GameState {
+    world: Vec<String>,
+    location: usize,
+    variables: HashMap<string, >,
+}
+
+fn game_loop(starting_world: Vec<String>) {
+    let mut state = GameState {
+        world: starting_world,
+        location: 1,
+        variables: HashMap,
+    };
+
     loop {
-        let line = &script[location];
+        let line = &state.world[state.location - 1];
 
         let first_char = match line.chars().next() {
             Some(character) => character,
@@ -39,7 +58,7 @@ fn game_loop(script: Vec<String>) {
             let command = line.replace('>', "");
             let mut split = command.split(" ");
             let operator = split.next().unwrap();
-            let status = runCommand(operator, &mut split, &mut location, &script);
+            let status = run_command(operator, &mut split, &mut state);
             match status {
                 Status::End => { return },
                 _ => {}
@@ -49,7 +68,7 @@ fn game_loop(script: Vec<String>) {
         }
 
         // End
-        location += 1;
+        state.location += 1;
     }
 }
 
@@ -62,15 +81,23 @@ fn print(string: &str) {
 
 enum Status {
     Continue,
-    End
+    End,
 }
 
-fn runCommand(operator: &str, params: &mut Split<&str>, location: &mut usize, script: &Vec<String>) -> Status {
+fn run_command(operator: &str, params: &mut Split<&str>, state: &mut GameState) -> Status {
     match operator {
         "end" => Status::End,
         "travel" => {
             let target = params.next().unwrap().parse::<usize>().unwrap();
-            *location = target - 1;
+            state.location = target - 1;
+            Status::Continue
+        },
+        "warp" => {
+            let world_name = params.next().unwrap();
+            let target = params.next().unwrap().parse::<usize>().unwrap();
+            let world = open_world(world_name);
+            state.world = world;
+            state.location = target - 1;
             Status::Continue
         },
         "choice" => {
